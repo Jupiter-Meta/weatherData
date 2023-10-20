be3e7fb0fbe66e876c3790a234804ab7aa774c28
saishibunb <144234378+saishibunb@users.noreply.github.com>
2023-10-20 09:16:20 +0530

    Create fetchWeather.py

---
+++ fetchWeather.py
@@ -0,0 +1,37 @@
+
+import requests
+import pymongo
+
+# OpenWeatherAPI configuration
+api_key = 'a1ce3564a0ea8c3c2bb353c83bc683a0'
+city_name = 'Hyderabad'  # Replace with the city you want to fetch weather data for
+api_url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}'
+api_aqi = http://api.openweathermap.org/data/2.5/air_pollution?lat=17.4345706&lon=78.3738571&appid=eb370f9ef90cb3375bb7c497f844f0c5
+# MongoDB configuration
+mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
+db = mongo_client['weather_data']
+collection = db['weather']
+
+def fetch_weather_data():
+    try:
+        response = requests.get(api_url)
+        data = response.json()
+        
+        if response.status_code == 200:
+            return data
+        else:
+            print(f"Error: {data['message']}")
+            return None
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return None
+
+def save_to_mongodb(weather_data):
+    if weather_data:
+        collection.insert_one(weather_data)
+        print("Weather data saved to MongoDB.")
+
+if __name__ == "__main__":
+    weather_data = fetch_weather_data()
+    if weather_data:
+        save_to_mongodb(weather_data)
